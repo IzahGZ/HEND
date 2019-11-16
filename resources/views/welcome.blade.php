bee20b1d90a85d9b03ca788b873aadb23f03ee8c
Izah Atirah <53392267+IzahGZ@users.noreply.github.com>
2019-11-16 12:42:37 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,6 +23,16 @@
             .full-height {
                 height: 100vh;
             }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
 
             .flex-center {
                 align-items: center;
